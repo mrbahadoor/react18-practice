ffc9b67ea544f55b7692707ba25ad772785e595c
Rushdi Bahadoor <my_name@example.com>
2024-10-27 12:22:47 +0400

    Conditional rendering with variable, arrow function or logical add

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,28 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-    const items = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-        'Houston',
-        'Phoenix',
-        'Philadelphia',
-        'San Antonio',
+    const items:string[] = [
+        // 'New York',
+        // 'Los Angeles',
+        // 'Chicago',
+        // 'Houston',
+        // 'Phoenix',
+        // 'Philadelphia',
+        // 'San Antonio',
     ];
 
+    // const message = items.length === 0 ? <p>No items to display</p> : null;
+
+    // const getMessage = () => {
+    //     return items.length === 0 ? <p>No items to display</p> : null;;
+    // }
+
+    // Use {..} to render items dynamically in JSX otherwise only html/xml allowed 
     return (
     <>
         <h1>List</h1>
+        {/* Logical add: true && 'Mosh' returns Mosh */}
+        { items.length === 0 && <p>No items to display</p> }
         <ul className="list-group">
             {items.map((item, index) => (
                 <li key={index} className="list-group-item">
